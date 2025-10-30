56a57856cbcaa6ac253b0e5cce8f35d0223deaff
d.eshenko <d.eshenko@psvgamestudio.com>
2025-10-30 12:27:29 +0200

    Release 4.4.1

--- react-native-cas.podspec
+++ react-native-cas.podspec
@@ -18,7 +18,7 @@ Pod::Spec.new do |s|
   s.public_header_files = "ios/**/*.h"
 
   s.frameworks = "UIKit", "Foundation"
-  s.dependency 'CleverAdsSolutions-Base', '~> 4.3.0'
+  s.dependency 'CleverAdsSolutions-Base', '~> 4.4.1'
 
   if defined?(install_modules_dependencies)
     install_modules_dependencies(s)
---
+++ version_updater.py
@@ -0,0 +1,67 @@
+import os
+
+_PLUGIN_VERSION = "4.4.1"
+_CAS_VERSION = "4.4.1"
+
+# Plugin publishing flow (from the project root):
+# python3 version_updater.py
+# yarn clean
+# yarn format
+# yarn lint
+# yarn codegen
+# yarn prepare
+# yarn release
+
+def update_version_in_file(file_path, prefix, suffix):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    success = False
+    with open(file_path, 'w', encoding='utf-8') as file:
+        for line in lines:
+            if line.startswith(prefix):
+                file.write(prefix + suffix + '\n')
+                success = True
+            else:
+                file.write(line)
+    
+    if success:
+        print("Updated " + file_path)
+    else:
+        raise RuntimeError(f"Prefix {prefix} not found in file: {file_path}")
+
+        
+def update_package_json():
+    update_version_in_file(
+        file_path=os.path.join("package.json"),
+        prefix='  "version": "',
+        suffix=_PLUGIN_VERSION + '",'
+    )
+    
+
+def update_podspec():   
+    update_version_in_file(
+        file_path=os.path.join("react-native-cas.podspec"),
+        prefix="  s.dependency 'CleverAdsSolutions-Base', '",
+        suffix=f"~> {_CAS_VERSION}'"
+    )
+
+def update_android_gradle_sdk():  
+    update_version_in_file(
+        file_path=os.path.join("android", "build.gradle"),
+        prefix='  implementation "com.cleveradssolutions:cas-sdk:',
+        suffix=_CAS_VERSION + '"'
+    )
+
+def update_android_gradle_example():  
+    update_version_in_file(
+        file_path=os.path.join("example", "android", "app", "build.gradle"),
+        prefix="  id 'com.cleveradssolutions.gradle-plugin' version '",
+        suffix=_CAS_VERSION + "'"
+    )
+      
+if __name__ == "__main__":
+    update_package_json()
+    update_podspec()
+    update_android_gradle_sdk()
+    update_android_gradle_example()
